4a5de1e1ff4c9f5f124dacbd05779afc9452286a
DankAvHi <thexcat86@gmail.com>
2023-11-15 12:32:53 +0300

    [crt] create note dto

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,1 +1,8 @@
-export class CreateNoteDto {}
+import { Prisma } from "@prisma/client";
+
+export class CreateNoteDto implements Prisma.NoteCreateInput {
+    createdAt?: string | Date | undefined;
+    updatedAt?: string | Date | undefined;
+    title?: string | null | undefined;
+    content: string;
+}
