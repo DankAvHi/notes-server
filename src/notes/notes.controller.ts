c604201b97679648463ab6d55a7a9c7d82804038
DankAvHi <thexcat86@gmail.com>
2023-11-15 14:03:20 +0300

    [format]

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,34 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { NotesService } from './notes.service';
-import { CreateNoteDto } from './dto/create-note.dto';
-import { UpdateNoteDto } from './dto/update-note.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import { NotesService } from "./notes.service";
+import { CreateNoteDto } from "./dto/create-note.dto";
+import { UpdateNoteDto } from "./dto/update-note.dto";
 
-@Controller('notes')
+@Controller("notes")
 export class NotesController {
-  constructor(private readonly notesService: NotesService) {}
+    constructor(private readonly notesService: NotesService) {}
 
-  @Post()
-  create(@Body() createNoteDto: CreateNoteDto) {
-    return this.notesService.create(createNoteDto);
-  }
+    @Post()
+    create(@Body() createNoteDto: CreateNoteDto) {
+        return this.notesService.create(createNoteDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.notesService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.notesService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.notesService.findOne(+id);
-  }
+    @Get(":id")
+    findOne(@Param("id") id: string) {
+        return this.notesService.findOne(+id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
-    return this.notesService.update(+id, updateNoteDto);
-  }
+    @Patch(":id")
+    update(@Param("id") id: string, @Body() updateNoteDto: UpdateNoteDto) {
+        return this.notesService.update(+id, updateNoteDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.notesService.remove(+id);
-  }
+    @Delete(":id")
+    remove(@Param("id") id: string) {
+        return this.notesService.remove(+id);
+    }
 }
