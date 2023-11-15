df556fe70bebe87909c0fc0aae89a3977cbf93f3
DankAvHi <thexcat86@gmail.com>
2023-11-15 14:19:26 +0300

    [add] realisation for crud operations on notes

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from "@nestjs/common";
 import { CreateNoteDto } from "./dto/create-note.dto";
 import { UpdateNoteDto } from "./dto/update-note.dto";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class NotesService {
-    create(createNoteDto: CreateNoteDto) {
-        return "This action adds a new note";
+    constructor(private readonly prisma: PrismaService) {}
+
+    async create(createNoteDto: CreateNoteDto) {
+        return await this.prisma.note.create({ data: createNoteDto });
     }
 
-    findAll() {
-        return `This action returns all notes`;
+    async findAll() {
+        return await this.prisma.note.findMany();
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} note`;
+    async findOne(id: number) {
+        return await this.prisma.note.findUnique({ where: { id } });
     }
 
-    update(id: number, updateNoteDto: UpdateNoteDto) {
-        return `This action updates a #${id} note`;
+    async update(id: number, updateNoteDto: UpdateNoteDto) {
+        return await this.prisma.note.update({ where: { id }, data: updateNoteDto });
     }
 
-    remove(id: number) {
-        return `This action removes a #${id} note`;
+    async remove(id: number) {
+        return await this.prisma.note.delete({ where: { id } });
     }
 }
