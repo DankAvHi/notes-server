f13bc203caebd9c46c9d46b2c7b234f46afd6162
DankAvHi <thexcat86@gmail.com>
2023-11-21 12:08:41 +0300

    [fix] note class

--- src/notes/entities/note.entity.ts
+++ src/notes/entities/note.entity.ts
@@ -1,9 +1,9 @@
 import { Note as NotePrisma } from "@prisma/client";
 
-export class Note implements NotePrisma {
+export class Note implements Partial<NotePrisma> {
     id: number;
     createdAt: Date;
     updatedAt: Date;
-    title: string | null;
+    title?: string | null;
     content: string;
 }
